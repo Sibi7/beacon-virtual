e6076ddb5573fd47bed0eeb654edfb6d8bff47ca
Sibi7 <Sibi_7@mail.ru>
2017-12-27 18:11:59 +0300

    add section partners

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,26 @@
 $(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
+    $('.reviews-carousel').owlCarousel({
         loop:true,
         margin:0,
         nav:true,
         navText: ["","next review"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+    $('.partners-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
         responsive:{
             0:{
                 items:1
